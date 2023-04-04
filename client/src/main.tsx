492b952f3cfb6c10c9f1ade77521d6831ecd70a5
Makoorr <azizbenrejeb@gmail.com>
2023-04-04 04:32:51 +0100

    uninstalled bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
-// Import our custom CSS
-import './scss/styles.scss'
-import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
