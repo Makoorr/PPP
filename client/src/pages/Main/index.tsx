220cea228319608402cfa457a69c66e0fe2b8bea
Makoorr <azizbenrejeb@gmail.com>
2023-04-05 05:03:48 +0100

    added wave and navbar components

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
+import Wave from '../../components/Wave';
+import Button from '../../components/Button';
+import Navbar from '../../components/Navbar';
 
 interface MainProps {}
 
@@ -14,7 +17,7 @@ export default function Main({}: MainProps) {
     // }
   
     return (
-      <p>
+      <>
         {/* Welcome {auth.user}!{" "}
         <button
           onClick={() => {
@@ -23,22 +26,31 @@ export default function Main({}: MainProps) {
         >
           Sign out
         </button> */}
-      </p>
+      </>
     );
   }
 
   return (
       <div>
          <AuthStatus />
-
-         {/* <ul>
-         <li>
-            <Link to="/login">Public Page</Link>
-         </li>
-         <li>
-            <Link to="/register">Protected Page</Link>
-         </li>
-         </ul> */}
+         <Wave nav ={<Navbar />}
+            left = {
+              <>
+                <h1>Task Management APP</h1>
+                <h2>DevOps PPP Project</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi qui eos a incidunt blanditiis doloribus temporibus neque? Corporis ipsa odit accusantium fugit mollitia porro cupiditate? Deserunt sapiente repudiandae esse aperiam.
+                </p>
+                <div className="gridwrapper">
+                  <div className="gridone"><Button width="auto">Login</Button></div>
+                  <div className="gridtwo"><Button width="auto">Register</Button></div>
+                </div>
+              </>
+            }
+            right= {
+              <img src="/illustration.svg" height="90%" alt="illustration" />
+            }
+            />
 
          <Outlet />
       </div>
