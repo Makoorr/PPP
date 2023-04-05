86904cfb9cb0d5e8b2b29e93ef4f42f8dd4922b0
Makoorr <azizbenrejeb@gmail.com>
2023-04-05 06:03:00 +0100

    started tasks

--- client/src/pages/Tasks/index.tsx
+++ client/src/pages/Tasks/index.tsx
@@ -1,11 +1,23 @@
 import React, { FC } from 'react';
+import './style.css'
+import Navbar from '../../components/Navbar';
 
 interface TasksProps {}
 
 export default function Tasks({}: TasksProps) {
    return (
-         <div>
-            <h1>Tasks</h1>
+      <div>
+         <Navbar background="True">
+            <li>
+            <a href="/">Home</a>
+            </li>
+         </Navbar>
+         <div className="sidenav">
+            <a>Tasks</a>
          </div>
+         <div className="contentnav">
+            <h1 color="#000">Tasks</h1>
+         </div>
+      </div>
    );
 }
