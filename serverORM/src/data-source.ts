2be744a77939886242fa25950f5601f843a91ec7
Makoorr <azizbenrejeb@gmail.com>
2023-04-07 07:16:11 +0100

    added dotenv

--- serverORM/src/data-source.ts
+++ serverORM/src/data-source.ts
@@ -4,15 +4,17 @@ import { Project } from "./entity/Project"
 import { Section } from "./entity/Section"
 import { Task } from "./entity/Task"
 import { User } from "./entity/User"
+import 'dotenv/config'
 
+// use dotEnv to get the environment variables
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "mysqldb",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "user",
-    password: "passworduser",
-    database: "ppp",
-    synchronize: true,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true, // false,
     logging: true,
     entities: [User, Project, Section, Task],
     subscribers: [],
