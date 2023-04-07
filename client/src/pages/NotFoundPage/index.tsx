e4374aa6ff94229ffb328c58513131ba197b1e37
Makoorr <azizbenrejeb@gmail.com>
2023-04-07 07:00:16 +0100

    removed a element

--- client/src/pages/NotFoundPage/index.tsx
+++ client/src/pages/NotFoundPage/index.tsx
@@ -23,7 +23,7 @@ const NotFoundPage = () => {
           <>
             <h1>404 - Page Not Found</h1>
           <p>The page you are looking for does not exist.</p>
-          <a><Link to="/">Go back to the homepage</Link></a>
+          <Link to="/">Go back to the homepage</Link>
           </>
         }
         right= {
