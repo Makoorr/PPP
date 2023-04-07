0a33771baa118568b8b1358a3b9927579fe25a2c
Makoorr <azizbenrejeb@gmail.com>
2023-04-07 06:02:32 +0100

    Removed unused component

--- client/src/components/TaskHolder/TaskHolder.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TaskHolder from './TaskHolder';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TaskHolder />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
