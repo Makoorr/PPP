a940fcf52e51ce2b9b3f8fdd210a22927583f2a6
Makoorr <azizbenrejeb@gmail.com>
2023-04-08 05:31:37 +0100

    added navbar content

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,18 +1,47 @@
 import React, { FC } from 'react';
 import { Logo, NavbarContainer } from './Navbar.styled';
+import { Link } from 'react-router-dom';
 
 interface NavbarProps {
-   children: React.ReactNode;
    background?: string;
 }
 
-const Navbar: FC<NavbarProps> = ({ children, background }) => (
-   <NavbarContainer background={ background }>
-      <Logo src="/vite.svg" alt="Logo" />
-      <ul>
-         { children }
-      </ul>
-  </NavbarContainer>
-);
+const Navbar: FC<NavbarProps> = ({ background }) => {
+   const isAuthenticated = localStorage.getItem('token') !== null;
+   return (
+      <NavbarContainer background={ background }>
+         <Link to="/"><Logo src="/vite.svg" alt="Logo" /></Link>
+         <ul>
+            {
+               isAuthenticated ? (
+                  <>
+                     <li>
+                        <a href="/">Home</a>
+                     </li>
+                     <li>
+                        <a href="/projects">Projects</a>
+                     </li>
+                     <li>
+                        <a href="/logout">Logout</a>
+                     </li>
+                  </>
+               ) : (
+                  <>
+                     <li>
+                     <a href="/">Home</a>
+                     </li>
+                     <li>
+                     <a href="/login">Login</a>
+                     </li>
+                     <li>
+                     <a href="/register">Register</a>
+                     </li>
+                  </>
+               )
+            }
+         </ul>
+      </NavbarContainer>
+   );
+};
 
 export default Navbar;
