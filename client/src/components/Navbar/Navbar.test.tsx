20473ff91cafc23ae9ab23115675bdbaefb1153c
Makoorr <azizbenrejeb@gmail.com>
2023-04-09 08:13:03 +0100

    removed unecessary files

--- client/src/components/Navbar/Navbar.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from '.';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Navbar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
