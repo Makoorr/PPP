ef128ce589d9281252b0790a1e2de9a35763f92f
Makoorr <azizbenrejeb@gmail.com>
2023-04-30 16:48:25 +0100

    updating handleSubmit for Registration

--- client/src/pages/Register/index.tsx
+++ client/src/pages/Register/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import Card from '../../components/Card';
+import axios from '../../axiosConfig';
 
 interface RegisterProps {}
 
@@ -20,17 +21,22 @@ export default function Register (props: RegisterProps) {
     event.preventDefault();
 
     let formData = new FormData(event.currentTarget);
-    let username = formData.get("username") as string;
+    const name = formData.get("username") as string;
+    const login = formData.get("login") as string;
+    const password = formData.get("password") as string;
 
-   //  auth.signin(username, () => {
-   //    // Send them back to the page they tried to visit when they were
-   //    // redirected to the register page. Use { replace: true } so we don't create
-   //    // another entry in the history stack for the register page.  This means that
-   //    // when they get to the protected page and click the back button, they
-   //    // won't end up back on the register page, which is also really nice for the
-   //    // user experience.
-   //    navigate(from, { replace: true });
-   //  });
+    axios
+      .post("/auth/register", {
+        name: name,
+        login: login,
+        password: password,
+      })
+      .then(function () {
+        navigate("/login");
+      })
+      .catch(function (error: any) {
+        alert(error);
+      });
   }
 
   return (
