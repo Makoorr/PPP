ab7bdb71037ba72270f548c03d2a486b38a61f57
ir0nbyte <ouachani.tuto@gmail.com>
2023-05-03 11:56:25 +0200

    addin

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -14,7 +14,7 @@ export default function Main({}: MainProps) {
         <h1>Task Management APP </h1>
         <h2>DevOps PPP Project</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi qui eos a incidunt blanditiis doloribus temporibus neque? Corporis ipsa odit accusantium fugit mollitia porro cupiditate? Deserunt sapiente repudiandae esse aperiam.
+        Effective management is crucial to the success of any organization. It involves planning, organizing, leading, and controlling resources to achieve the organization's goals and objectives. Good managers are able to inspire and motivate their teams to work together towards a common goal, while also ensuring that all tasks are completed efficiently and effectively. They must also be able to adapt to changing circumstances and make informed decisions in a timely manner. At the heart of effective management is the ability to communicate clearly and build positive relationships with colleagues, employees, customers, and stakeholders. Whether you're running a small business or a large corporation, investing in good management practices can help you achieve your goals, improve productivity, and ultimately drive success.
         </p>
         <div className="gridwrapper">
           <div className="gridone"><Link to='/login'><Button width="auto">Login</Button></Link></div>
