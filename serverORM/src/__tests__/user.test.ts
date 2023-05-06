aef6f148dd4624a540bb3d1a19381f018fb712f1
Makoorr <azizbenrejeb@gmail.com>
2023-05-06 17:33:48 +0100

    added authentication testing

--- serverORM/src/__tests__/user.test.ts
+++ serverORM/src/__tests__/user.test.ts
@@ -1,55 +1,47 @@
-// import { UserController } from '../controller/UserController';
 import createServer from '../server';
 import { AppDataSource } from '../data-source'
 import { User } from '../entity/User'
 import { Request, Response, NextFunction } from 'express';
 import * as supertest from 'supertest'
-const request = require('supertest')
 
 const app = createServer();
 
 const userPayload = {
-    login: 'test',
-    password: 'testpassword'
+    login: 'jane.doe',
+    password: 'password'
 }
 
 const userInput = {
-    login: "Jane Doe",
+    login: "jane.doe",
     password: "password",
-    name: "jane.doe",
+    name: "Jane Doe",
   };
-
 
 describe('User', () => {
     describe('register User', () => {
         describe('given Valid login and password', () => {
-            // let user = AppDataSource.getRepository(User);
-            
-            // it('should return a signed token', async () => {
-            //     const createUserServiceMock = jest.spyOn(user, 'save')
-            //     // @ts-ignore
-            //     .mockResolvedValueOnce(userPayload)
-
-            //     const { statusCode, body } = await supertest(app)
-            //     .post("/auth/register")
-            //     .send(userInput);
-
-            //     expect(statusCode).toBe(200);
-            //     // expect(body).toEqual(userPayload);
-            //     // expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
-            // })
-
             describe('Authentication', () => {
                 let token: string;
               
                 it('should authenticate a user and return a token', async () => {
-                  const response = await request(app)
-                    .post('/auth/register') // Assuming you have a login route in your app
+                    // Mock the findOne method to simulate no existing user
+                    const findOneMock = jest
+                    .spyOn(AppDataSource.manager, 'findOne')
+                    .mockResolvedValueOnce(undefined);
+
+                    // Mock the save method to simulate successful user creation
+                    const saveMock = jest
+                        .spyOn(AppDataSource.manager, 'save')
+                        .mockResolvedValueOnce(userPayload);
+
+                  const response = await supertest(app)
+                    .post('/auth/register')
                     .send(userInput);
               
                   expect(response.status).toBe(200);
-                  expect(response.body).toEqual("User created.");
-              
+                  expect(response.body).toEqual({"message": "User created."});
+                  expect(saveMock).toHaveBeenCalledWith(userInput);
+
                   token = response.body.token; // Store the token for future requests
                 });
             })
