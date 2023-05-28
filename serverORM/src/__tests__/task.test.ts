dfe1b518d932162fc8b837c78b00f776a9e13567
Makoorr <azizbenrejeb@gmail.com>
2023-05-28 21:29:10 +0100

    added task testing

--- serverORM/src/__tests__/task.test.ts
+++ serverORM/src/__tests__/task.test.ts
@@ -11,11 +11,16 @@ const app = createServer();
 jest.mock('../controller/TaskController', () => {
     return {
       TaskController: jest.fn().mockImplementation(() => ({
-        all: jest.fn(),
-        one: jest.fn(),
-        save: jest.fn(),
-        update: jest.fn(),
-        remove: jest.fn(),
+        all: jest.fn()
+        .mockImplementation(() => Promise.resolve([taskInput])),
+        one: jest.fn()
+        .mockImplementation(() => Promise.resolve(taskInput)),
+        save: jest.fn()
+        .mockImplementation(() => Promise.resolve(true)),
+        update: jest.fn()
+        .mockImplementation(() => Promise.resolve(true)),
+        remove: jest.fn()
+        .mockImplementation(() => Promise.resolve("task has been removed")),
       })),
     };
   });
@@ -33,8 +38,9 @@ const taskInput = {
 
 let token: string; // token to be used in tests
 
-describe('Authenticated Routes', () => {
-    beforeAll (async () => {
+describe('Authenticated Routes', () => {    
+    describe('Task Routes', () => {
+      beforeEach (async () => {
         // Mock the findOne method to simulate an existing user
         const findOneMock = jest
         .spyOn(AppDataSource.manager, 'findOne')
@@ -47,30 +53,119 @@ describe('Authenticated Routes', () => {
     
         expect(findOneMock).toHaveBeenCalled();
         token = response.body.token;
-        console.log('token_project: '+token)
       });
-    
-    describe('Task Routes', () => {
-        describe('GET /task', () => {
-            it('should return all tasks', async () => {
-                // const mockInstance = new TaskController();
 
-                console.log('token_verif TASK: '+token);
-                request(app)
-                .get('/task')
-                .set('Authorization', `Bearer ${token}`)
-                .expect(200);
-
-                // .end(function(err, res) {
-                //     if (err) throw err;
-                //     console.log(res.body);
-                // });
-
-                // expect(mockInstance.all).toHaveBeenCalled();
-                
-                // expect(response.status).toBe(200);
-                // expect(response.body).toEqual(taskInput);
-            });
+      describe('get all tasks', () => {
+        it('should return all tasks', async () => {
+          await request(app)
+            .get('/task')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
         });
+      });
+
+      describe('get one task', () => {
+          it('should return one task', async () => {
+            await request(app)
+              .get('/task/1')
+              .set('Authorization', `Bearer ${token}`)
+              .expect(200);
+          });
+      });
+      
+      describe('create task', () => {
+          it('should create a task', async () => {
+            await request(app)
+              .post('/task')
+              .set('Authorization', `Bearer ${token}`)
+              .send(taskInput)
+              .expect(200);
+
+              expect(TaskController).toHaveBeenCalled();
+          });
+      });
+      
+      describe('update task', () => {
+          it('should update a task', async () => {
+            const taskId = 1;
+            const updatedTaskData = {
+              name: 'Updated Task Name',
+              description: 'Updated Task Description',
+              priority: 3,
+              deadline: '2023-06-30',
+            };
+        
+            await request(app)
+              .put(`/task/${taskId}`)
+              .set('Authorization', `Bearer ${token}`)
+              .send(updatedTaskData)
+              .expect(200);
+
+            expect(TaskController).toHaveBeenCalled();
+          });
+        
+      });
+
+      describe('remove task', () => {
+        it('should remove a task', async () => {
+          const taskId = 1;
+      
+          await request(app)
+            .delete(`/task/${taskId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
+      
+          expect(TaskController).toHaveBeenCalled();
+        });
+      });
+      
     });
+});
+
+describe('Unauthenticated Routes', () => {
+  describe('get Task Route', () => {
+    it('should not let you get all tasks', async () => {
+      await request(app)
+        .get('/task')
+        .expect(401);
+    })
+    it('should not let you get one of the tasks', async () => {
+      await request(app)
+        .get('/task/1')
+        .expect(401);
+    })
+  });
+  describe('create Task Route', () => {
+    it('should not let you create a task', async () => {
+      await request(app)
+        .post('/task')
+        .send(taskInput)
+        .expect(401);
+    })
+  });
+  describe('update Task Route', () => {
+    it('should not let you update a task', async () => {
+      const taskId = 1;
+      const updatedTaskData = {
+        name: 'Updated Task Name',
+        description: 'Updated Task Description',
+        priority: 3,
+        deadline: '2023-06-30',
+      };
+  
+      await request(app)
+        .put(`/task/${taskId}`)
+        .send(updatedTaskData)
+        .expect(401);
+    })
+  });
+  describe('remove Task Route', () => {
+    it('should not let you remove a task', async () => {
+      const taskId = 1;
+  
+      await request(app)
+        .delete(`/task/${taskId}`)
+        .expect(401);
+    })
+  });
 });
